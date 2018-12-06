b29aa0788b3d4c401f591c136935ab7743a8bf2d
azuregomez <edgome@microsoft.com>
2018-12-06 13:17:18 -0600

    uri

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -1,15 +1,16 @@
 $rgname = "ASE01-RG"
 $location = "North Central US"
-#$templateuri = "https://raw.githubusercontent.com/azuregomez/pan-g-hub/master/azuredeploy.json"
-$templatefile = "C:\projects\github\appservice\azuredeploy.json"
-$templateparamfile = "C:\projects\github\appservice\azuredeploy.parameters.json"
+$templateuri = "https://raw.githubusercontent.com/azuregomez/appservice/master/azuredeploy.json"
+$templateparamuri = "https://raw.githubusercontent.com/azuregomez/appservice/master/azuredeploy.parameters.json"
+#$templatefile = "C:\projects\github\appservice\azuredeploy.json"
+#$templateparamfile = "C:\projects\github\appservice\azuredeploy.parameters.json"
 $rg = get-azurermresourcegroup -location $location -name $rgname
 if ($null -eq $rg)
 {
     new-azurermresourcegroup -location $location -name $rgname
 }
 # deploy 
-# New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri $templateuri -TemplateParameterFile $templateparamfile 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
-# Test-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri $templateuri -TemplateParameterUri $templateparamuri
+#New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
+
 
