56b069a3e7f40b0911a6b2000ef85e5982ba16eb
azuregomez <edgome@microsoft.com>
2019-01-16 14:15:17 -0600

    redis

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -1,16 +1,16 @@
-$rgname = "SEcurePaaS-RG"
+$rgname = "PaaS2-RG"
 $location = "North Central US"
 $templateuri = "https://raw.githubusercontent.com/azuregomez/appservice/master/azuredeploy.json"
 $templateparamuri = "https://raw.githubusercontent.com/azuregomez/appservice/master/azuredeploy.parameters.json"
-#$templatefile = "C:\projects\github\appservice\azuredeploy.json"
-#$templateparamfile = "C:\projects\github\appservice\azuredeploy.parameters.json"
+$templatefile = "C:\projects\github\appservice\azuredeploy.json"
+$templateparamfile = "C:\projects\github\appservice\azuredeploy.parameters.json"
 $rg = get-azurermresourcegroup -location $location -name $rgname
 if ($null -eq $rg)
 {
     new-azurermresourcegroup -location $location -name $rgname
 }
 # deploy 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri $templateuri -TemplateParameterUri $templateparamuri
-#New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
+#New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateUri $templateuri -TemplateParameterUri $templateparamuri
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
 
 
